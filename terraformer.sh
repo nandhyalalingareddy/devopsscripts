6bc775a5e74f8e24bb830d38ded376d37b25c44b
nandhyalalingareddy <120440033+nandhyalalingareddy@users.noreply.github.com>
2025-03-19 20:13:33 +0530

    Update terraformer.sh

--- terraformer.sh
+++ terraformer.sh
@@ -1,4 +1,8 @@
-wget https://github.com/GoogleCloudPlatform/terraformer/releases/download/0.8.24/terraformer-all-linux-amd64
-chmod +x terraformer-all-linux-amd64
-sudo mv terraformer-all-linux-amd64 /usr/local/bin/terraformer
+sudo yum update -y
+sudo yum install -y wget unzip
+wget https://github.com/GoogleCloudPlatform/terraformer/releases/download/0.8.24/terraformer-aws-linux-amd64 -O terraformer
+chmod +x terraformer
+sudo mv terraformer /usr/local/bin/
+echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+source ~/.bashrc
 terraformer --version
